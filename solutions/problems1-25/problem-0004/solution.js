d319e5cacb86a142120f4e1b7c2b5a0b0a2e7758
Roland Burrows <rolandnburrows@gmail.com>
2021-01-06 00:52:46 -0700

    Solve problem 0004

---
+++ solutions/problems1-25/problem-0004/solution.js
@@ -0,0 +1,32 @@
+console.log('Running solution for Problem 0004...');
+
+// challenge settings
+const digitsLength = 3;  // test with 2 first
+
+// start at maximum values for products
+var startNum = 9;
+startNum = "9".repeat(digitsLength - String(startNum).length) + startNum;
+var floorNum = "9".repeat(digitsLength - 1);  // no point in calculating beyond this limit
+var productSourceOne = startNum;
+var productSourceTwo = startNum;
+
+var productVals = new Map();
+// build map of all products and their contributing values
+for (numOne = productSourceOne; numOne > floorNum; numOne--) {
+  for (numTwo = productSourceTwo; numTwo > floorNum; numTwo--) {
+    var product = numOne * numTwo;
+    var reversedProduct = String(product).split("").reverse().join("");
+    if (String(product) === reversedProduct) {
+      // palindrome detected!
+      productVals.set(product, `${numOne} x ${numTwo}`);
+    }
+  }
+}
+
+// extract the winner
+const highestPalindrome = Math.max(...productVals.keys());
+const palindromeContributors = productVals.get(highestPalindrome);
+
+console.log(`Answer: ${highestPalindrome} == ${palindromeContributors}`);
+
+// Answer: 906609 == 913 x 993
