cb7ebbbe0e4adabacb977ff5f060232e1f4a1be8
Roland Burrows <rolandnburrows@gmail.com>
2021-02-27 14:08:16 -0700

    Add tests and improve performance

--- solutions/problems51-75/problem-0053/solution.js
+++ solutions/problems51-75/problem-0053/solution.js
@@ -6,31 +6,36 @@ const searchLimit = 1000000;
 const nLimit = 100;
 
 function bigIntFactorial(number) {
-  var num = BigInt(number);
-  for (var i = (num - BigInt(1)); i > 1; i--) {
-    num *= i;
+  if (number === 0) {
+    return BigInt(1);  // 0! === 1
   }
-  return num;
+  var result = 1;
+  for (var i = 2; i <= number; i++) {
+    result *= i;
+  }
+  return BigInt(result);
 }
+console.assert(bigIntFactorial(10) === BigInt(3628800));
 
 function combinatoricsChoose(n, r) {
   var numerator = bigIntFactorial(n);
   var denominator = (bigIntFactorial(r)*bigIntFactorial(n-r));
-  // 0! === 1
+  // N choose 0 === 1, avoid divide by zero
   if (denominator === BigInt(0)) {
     return 1;
   };
   var result = numerator / denominator;
   return result;
 }
+console.assert(combinatoricsChoose(10,0) === BigInt(1));
+console.assert(combinatoricsChoose(15,1) === BigInt(15));
+console.assert(combinatoricsChoose(23,10) === BigInt(1144066));
+console.assert(combinatoricsChoose(100,100) === BigInt(1));
 
 
 var counter = 0;
 for (var n = 1; n <= nLimit; n++) {
   for (var r = 0; r <= n; r++) {
-    // if (n === 23 && r === 10) {
-    //   console.log(n, r, combinatoricsChoose(n, r));
-    // }
     if (combinatoricsChoose(n, r) > searchLimit) {
       counter++;
     }
@@ -45,4 +50,4 @@ console.timeLog('run time');
 
 // How many, not necessarily distinct, values of (n, r) <= 100 are greater than one-million?
 // Answer: 4075
-// run time: 102.606ms
+// run time: 8.245ms
